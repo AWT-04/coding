fe67a0c9014de42667cb93b8935c03c630e58c46
ramalaso <osalamar@gmail.com>
2019-10-31 15:05:12 -0400

    Added EAN Validator

---
+++ src/main/java/com/company/EANValidator.java
@@ -0,0 +1,19 @@
+package com.company;
+
+class EANValidator {
+
+    private EANValidator() {
+    }
+
+    static boolean validate(final String eanCode) {
+        int sum = 0;
+        int num = 0;
+        for (int i = 0; i < eanCode.length()-1; i++) {
+            num = Integer.parseInt(String.valueOf(eanCode.charAt(i)));
+            sum = i % 2 == 0 ?  sum + num*1 : sum + num*3;
+        }
+        int chesumCalculated = 10 - sum % 10 <= 9 ? 10 - sum % 10 : 0;
+        int lastValue = Integer.parseInt(String.valueOf(eanCode.charAt(eanCode.length()-1)));
+        return chesumCalculated == lastValue;
+    }
+}
