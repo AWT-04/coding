30f20afeb018f77b0e820367f7c6ea27cd470fe8
Andy <Bazualdo>
2019-11-04 12:19:33 -0400

    appling refactor based on comments

--- src/main/java/org/fundacionjala/coding/andy/EANValidator.java
+++ src/main/java/org/fundacionjala/coding/andy/EANValidator.java
@@ -11,21 +11,14 @@ public final class EANValidator {
         int sum = 0;
         final int constant = 10;
         final int constant2 = 3;
-        boolean band = false;
         for (int i = 1; i <= (eanCode.length() - 1); i++) {
-            int factor = 1;
-            if (i % 2 == 0) {
-                factor = constant2;
-            }
-            sum = sum +  (Character.getNumericValue(eanCode.charAt(i - 1)) * factor);
+            int factor = i % 2 == 0 ? constant2 : 1;
+            sum += (Character.getNumericValue(eanCode.charAt(i - 1)) * factor);
         }
         sum = constant - (sum % constant);
         if (sum >= constant) {
             sum = sum % constant;
         }
-        if (Character.getNumericValue(eanCode.charAt((eanCode.length() - 1))) == sum) {
-            band = true;
-        }
-        return band;
+        return (Character.getNumericValue(eanCode.charAt((eanCode.length() - 1))) == sum);
     }
 }
