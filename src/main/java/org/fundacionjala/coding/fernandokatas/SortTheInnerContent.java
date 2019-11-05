6d3090e858ecb57577c81b8ffb2c7a3c59d6090f
Fernando Hinojosa <fernando.hinojosa@live.com>
2019-11-05 10:12:38 -0400

    Resolved conflicts

--- src/main/java/org/fundacionjala/coding/fernandokatas/SortTheInnerContent.java
+++ src/main/java/org/fundacionjala/coding/fernandokatas/SortTheInnerContent.java
@@ -2,20 +2,21 @@ package org.fundacionjala.coding.fernandokatas;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.StringJoiner;
 
 public final class SortTheInnerContent {
+    private static final int SIZE = 3;
+
     private SortTheInnerContent() {
     }
 
-    public static String sortTheInnerContent(String words) {
+    public static String sortTheInnerContent(final String words) {
         String[] sortWord = words.split(" ");
         StringJoiner join = new StringJoiner(" ");
         String[] result;
         for (String listWord : sortWord) {
             result = listWord.split("");
-            if (result.length > 3) {
+            if (result.length > SIZE) {
                 Arrays.sort(result, 1, result.length - 1,
                         Collections.reverseOrder());
             }
